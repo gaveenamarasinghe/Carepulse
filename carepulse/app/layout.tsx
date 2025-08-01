b9e603aecbe530a68d3cb17517e054661399169a
Gaveen Amarasinghe <gaveenamarasinghe2004@gmail.com>
2025-08-01 10:40:44 +0530

    update files

--- carepulse/app/layout.tsx
+++ carepulse/app/layout.tsx
@@ -2,15 +2,14 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-import { cn } from '@/lib/utils'
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "next-themes";
 
 const fontSans = Plus_Jakarta_Sans({
   variable: "--font-sans",
   subsets: ["latin"],
-  weight:['300' ,'400' ,'500' ,'600' ,'700'],
+  weight: ["300", "400", "500", "600", "700"],
 });
-
-
 
 export const metadata: Metadata = {
   title: "CAREPULSE",
@@ -23,11 +22,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={cn('min-h-screen bg-dark-300 front-sans antialiased', fontSans.variable)}
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
