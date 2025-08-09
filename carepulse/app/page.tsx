028dd1314103924d315987eda29fef12a815356d
Gaveen Amarasinghe <gaveenamarasinghe2004@gmail.com>
2025-08-09 17:53:56 +0530

    add and update codes

--- carepulse/app/page.tsx
+++ carepulse/app/page.tsx
@@ -1,42 +1,53 @@
-import { PatientForm } from "@/components/forms/PatientForm";
 import Image from "next/image";
-import Link from "next/link"; // Import the Link component
+import Link from "next/link";
 
-export default function Home() {
+import { PatientForm } from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
+
+type SearchParamProps = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const params = await searchParams; // Await the searchParams Promise
+  const isAdmin = params?.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
+      {isAdmin && <PasskeyModal />}
 
-
-
-
-      
       <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
+        <div className="sub-container max-w-[ салют496px]">
           <Image
             src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
             alt="patient"
-            height={200}
-            width={200}
             className="mb-12 h-10 w-fit"
           />
+
           <PatientForm />
+
           <div className="text-14-regular mt-20 flex justify-between">
-            <p>© 2024 CarePulse</p>
-            <Link href="/admin=true" className="text-green-500">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePluse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
               Admin
             </Link>
           </div>
         </div>
       </section>
 
-    <Image
-            src="/assets/images/onboarding-img.png"
-            height={1000}
-            width={1000}
-             alt="patient"
-            className="side-img max-w-[50%]"
-          />
-
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
-}
+};
+
+export default Home;
